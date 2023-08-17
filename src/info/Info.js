e7d732e4a8f3b0b683feada76e13920d53fe93cc
Ijaz Liaqat <ijaz.liaqat@ceative.co.uk>
2023-08-17 21:23:38 +0500

    changes in project file

--- src/info/Info.js
+++ src/info/Info.js
@@ -84,36 +84,13 @@ export const info = {
     exposedTo: ["node js", "express js", "mongodb"],
   },
   portfolio: [
-    // This is where your portfolio projects will be detailed
-    // {
-    //   title: "Project 1",
-    //   live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-    //   source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-    //   image: mock1,
-    // },
-    // {
-    //   title: "Project 2",
-    //   live: "https://paytonpierce.dev",
-    //   source: "https://github.com/paytonjewell",
-    //   image: mock2,
-    // },
-    // {
-    //   title: "Project 3",
-    //   live: "https://paytonpierce.dev",
-    //   source: "https://github.com/paytonjewell",
-    //   image: mock3,
-    // },
-    // {
-    //   title: "Project 4",
-    //   live: "https://paytonpierce.dev",
-    //   source: "https://github.com/paytonjewell",
-    //   image: mock4,
-    // },
-    // {
-    //   title: "Project 5",
-    //   live: "https://paytonpierce.dev",
-    //   source: "https://github.com/paytonjewell",
-    //   image: mock5,
-    // },
+
+    {
+      title: "Share My Dine",
+      live: "https://app-dev.sharemydine.co.uk/authentication/signin", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+      image: mock1,
+    },
+    
   ],
 };
